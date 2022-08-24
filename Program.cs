32184bd8e54bc81703dd4a1ff470a6dcb5adae78
Administrator <Administrator@PC-202107151145>
2022-08-25 01:25:07 +0800

    post

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿
+using DotNet.Utilities;
+using Newtonsoft.Json;
+using SufeiNet;
+using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ConsoleApp8
@@ -7,11 +12,43 @@ namespace ConsoleApp8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, string> dic = postdouyin();
             using (StreamWriter sw = new StreamWriter("README.md", true))
             {
-                sw.WriteLine("\r\n" + DateTime.Now.ToString("HH:mm:ss") + "\r\n");
+                foreach (var item in dic)
+                {
+                    sw.WriteLine(item.Key+"|"+item.Value+"\r\n");
+                }
+                
             }
+        }
+
+        public static Dictionary<string, string> postdouyin()
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            HttpHelper hh = new HttpHelper();
+            HttpItem hi = new HttpItem();
+            hi.URL = "https://aweme.snssdk.com/aweme/v1/hot/search/list/?detail_list=1";
+            string html = hh.GetHtml(hi);
+            SufeiNet_Test rb = JsonConvert.DeserializeObject<SufeiNet_Test>(html);
+            if (rb.Data!=null)
+            {
+                if (rb.Data.WordList!=null&& rb.Data.WordList.Count>0)
+                {
+                    foreach (var item in rb.Data.WordList)
+                    {
+                        if (item.HotValue!=null&& item.Word!=null)
+                        {
+                            if (!dic.ContainsKey(item.Word))
+                            {
+                                dic.Add(item.Word, item.HotValue);
+                                Console.WriteLine("关键字："+ item.Word+"  ----  热度："+ item.HotValue);
+                            }
+                        }
+                    }
+                }
+            }
+            return dic;
         }
     }
 }
---
+++ json.cs
@@ -0,0 +1,240 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace SufeiNet
+{
+
+    public class WordCover
+    {
+
+        [JsonProperty("url_list")]
+        public IList<string> UrlList;
+
+        [JsonProperty("uri")]
+        public string Uri;
+    }
+
+    public class WordList
+    {
+
+        [JsonProperty("drift_info")]
+        public object DriftInfo;
+
+        [JsonProperty("hotlist_param")]
+        public string HotlistParam;
+
+        [JsonProperty("discuss_video_count")]
+        public int DiscussVideoCount;
+
+        [JsonProperty("hot_value")]
+        public string HotValue;
+
+        [JsonProperty("challenge_id")]
+        public string ChallengeId;
+
+        [JsonProperty("word_sub_board")]
+        public IList<int> WordSubBoard;
+
+        [JsonProperty("aweme_infos")]
+        public object AwemeInfos;
+
+        [JsonProperty("word")]
+        public string Word;
+
+        [JsonProperty("label")]
+        public int Label;
+
+        [JsonProperty("position")]
+        public int Position;
+
+        [JsonProperty("sentence_id")]
+        public string SentenceId;
+
+        [JsonProperty("event_time")]
+        public int EventTime;
+
+        [JsonProperty("related_words")]
+        public object RelatedWords;
+
+        [JsonProperty("sentence_tag")]
+        public int SentenceTag;
+
+        [JsonProperty("article_detail_count")]
+        public int ArticleDetailCount;
+
+        [JsonProperty("word_cover")]
+        public WordCover WordCover;
+
+        [JsonProperty("video_count")]
+        public int VideoCount;
+
+        [JsonProperty("group_id")]
+        public string GroupId;
+
+        [JsonProperty("word_type")]
+        public int WordType;
+
+        [JsonProperty("can_extend_detail")]
+        public bool? CanExtendDetail;
+
+        [JsonProperty("display_style")]
+        public int? DisplayStyle;
+
+        [JsonProperty("room_count")]
+        public int? RoomCount;
+
+        [JsonProperty("emo_label")]
+        public int? EmoLabel;
+    }
+
+    public class ShareInfo
+    {
+
+        [JsonProperty("share_url")]
+        public string ShareUrl;
+
+        [JsonProperty("share_title")]
+        public string ShareTitle;
+
+        [JsonProperty("share_link_desc")]
+        public string ShareLinkDesc;
+    }
+
+    public class WordCover2
+    {
+
+        [JsonProperty("url_list")]
+        public IList<string> UrlList;
+
+        [JsonProperty("uri")]
+        public string Uri;
+    }
+
+    public class TrendingList
+    {
+
+        [JsonProperty("event_time")]
+        public int EventTime;
+
+        [JsonProperty("hotlist_param")]
+        public string HotlistParam;
+
+        [JsonProperty("aweme_infos")]
+        public object AwemeInfos;
+
+        [JsonProperty("hot_value")]
+        public string HotValue;
+
+        [JsonProperty("video_count")]
+        public int VideoCount;
+
+        [JsonProperty("word_type")]
+        public int WordType;
+
+        [JsonProperty("sentence_id")]
+        public string SentenceId;
+
+        [JsonProperty("discuss_video_count")]
+        public int DiscussVideoCount;
+
+        [JsonProperty("word")]
+        public string Word;
+
+        [JsonProperty("group_id")]
+        public string GroupId;
+
+        [JsonProperty("word_cover")]
+        public WordCover2 WordCover;
+
+        [JsonProperty("related_words")]
+        public object RelatedWords;
+
+        [JsonProperty("drift_info")]
+        public object DriftInfo;
+
+        [JsonProperty("sentence_tag")]
+        public int SentenceTag;
+
+        [JsonProperty("word_sub_board")]
+        public object WordSubBoard;
+
+        [JsonProperty("label")]
+        public int Label;
+
+        [JsonProperty("display_style")]
+        public int? DisplayStyle;
+
+        [JsonProperty("article_detail_count")]
+        public int? ArticleDetailCount;
+
+        [JsonProperty("room_count")]
+        public int? RoomCount;
+    }
+
+    public class Data
+    {
+
+        [JsonProperty("trending_desc")]
+        public string TrendingDesc;
+
+        [JsonProperty("recommend_list")]
+        public object RecommendList;
+
+        [JsonProperty("word_list")]
+        public IList<WordList> WordList;
+
+        [JsonProperty("active_time")]
+        public string ActiveTime;
+
+        [JsonProperty("share_info")]
+        public ShareInfo ShareInfo;
+
+        [JsonProperty("trending_list")]
+        public IList<TrendingList> TrendingList;
+    }
+
+    public class Extra
+    {
+
+        [JsonProperty("now")]
+        public long Now;
+
+        [JsonProperty("fatal_item_ids")]
+        public IList<object> FatalItemIds;
+
+        [JsonProperty("logid")]
+        public string Logid;
+    }
+
+    public class LogPb
+    {
+
+        [JsonProperty("impr_id")]
+        public string ImprId;
+    }
+
+    public class SufeiNet_Test
+    {
+
+        [JsonProperty("status_code")]
+        public int StatusCode;
+
+        [JsonProperty("data")]
+        public Data Data;
+
+        [JsonProperty("ad_search_list")]
+        public object AdSearchList;
+
+        [JsonProperty("board_tabs")]
+        public object BoardTabs;
+
+        [JsonProperty("extra")]
+        public Extra Extra;
+
+        [JsonProperty("log_pb")]
+        public LogPb LogPb;
+    }
+
+}
