a7f11a02ff28f6175cb30e7b40fdd2debb3e2a96
wzz1190 <79913593+wzz1190@users.noreply.github.com>
2022-08-31 00:20:01 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -366,7 +366,7 @@ namespace ConsoleApp8
         {
             using (StreamWriter sw = new StreamWriter(txt, true, Encoding.Default))
             {
-                sw.WriteLine(idd);
+                sw.WriteLine(idd + Environment.NewLine);
             }
         }
 
@@ -376,7 +376,7 @@ namespace ConsoleApp8
             {
                 foreach (var item in lss)
                 {
-                    string txtl = item.work + "|" + item.hot + "|" + item.ID;
+                    string txtl = item.work + "|" + item.hot + "|" + item.ID + Environment.NewLine;
                     sw.WriteLine(txtl);
                 }
 
